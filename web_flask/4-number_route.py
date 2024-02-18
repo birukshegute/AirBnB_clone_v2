1d1b91226e0f09c511b61645d92921a1ec122a31
birukshegute <birukshegute@gmail.com>
2024-02-18 12:31:07 -0800

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" task 1 for / and /hbnb
-/c/<text>: display “C ”, followed by the value of the text variable.
-/python/<text>: display Python {the value of the text variable}"""
+""" It is the same as task 3,
+Additional:  display “n is a number” only if n is an integer"""
 
 from flask import Flask
 
@@ -12,29 +11,36 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-    """ Hello world function """
+    """ hello world fuction """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ Hbnb function """
+    """ hbnb function"""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_is_cool(text):
-    """ C is fun function """
+def c_is_fun(text):
+    """ c is fun function """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
-def python_is_cool(text='is cool'):
-    """ python is fun function """
+def python_is_fun(text='is cool'):
+    """ Python is fun function """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>')
+def n_is_a_number(n):
+    """ n is a number function"""
+    n = str(n)
+    return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':

72d02dec3d64c4205f2389c7df2469c9a3fa2ec2
birukshegute <birukshegute@gmail.com>
2024-02-18 11:59:03 -0800

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ hello world fuction """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello():
+    """ hello wrold function """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ c is fun function """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

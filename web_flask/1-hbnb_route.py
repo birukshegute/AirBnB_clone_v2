7aa323a67218ae994087220d1e60bdde7f5943f4
birukshegute <birukshegute@gmail.com>
2024-02-18 11:48:38 -0800

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Web application listening on 0.0.0.0, port 5000
+/: display “Hello HBNB!
+/hbnb: display “HBNB”
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Hello world functiom """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ HBNb function"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

46b5b3a0d21390ace7c4a6ef924cb85250807229
birukshegute <birukshegute@gmail.com>
2024-02-21 05:45:38 -0800

    task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" a Fabric script that creates and distributes
+an archive to your web servers"""
+
+from fabric.api import *
+from datetime import datetime
+from os.path import exists
+
+
+env.hosts = ['100.26.222.187', '54.157.173.233']
+
+
+def do_pack():
+    """ Method to pack the folder contents of web static """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """ function to perform the task"""
+    if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        return True
+    except:
+        return False
